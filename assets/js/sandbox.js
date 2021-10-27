1223dc8c39c0de82525b32bf4bf7c2c174e7e558
danil <minakonn9shkoni@gmail.com>
2021-10-27 17:09:43 +0300

    Add adder

--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -1,10 +1,20 @@
 "use strict";
 
-function createCounter() {
-  let i = 0;
-  return function counter() {
-    i++;
-    return i;
+function createCounter(initial = 0) {
+  let i = initial; //замкнутая переменная//
+  return {
+    increment() {
+      return ++i;
+    },
+    decrement() {
+      return --i;
+    },
   };
 }
 const counter1 = createCounter();
+const counter2 = createCounter();
+
+const createAdder = (n) => (m) => (n += m);
+
+const adder = createAdder(10);
+console.log(adder(20));
