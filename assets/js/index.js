e15716c8bf4b377c99def831ddf528110ab6bf90
danil <minakonn9shkoni@gmail.com>
2021-10-28 12:25:48 +0300

    Add dipping for btnHandler

--- assets/js/index.js
+++ assets/js/index.js
@@ -73,12 +73,23 @@
 //   btn.addEventListener("click", btnHandler)
 // }
 const btns = document.querySelectorAll('button');
-const root = document.getElementById('root');
+// const root = document.getElementById('root');
 
 const btnHandler = ({target:{dataset:{color},parentNode:parentRoot}})=>{
   parentRoot.style.backgroundColor = color;
-}
 
+}
+const handler = ({currentTarget})=>{
+
+  console.log(currentTarget, 'dipping');
+}
 for(const btn of btns){
   btn.addEventListener('click', btnHandler)
+  btn.addEventListener('click', handler, true)
 }
+document.getElementById('root').addEventListener('click',handler, true);
+document.body.addEventListener('click',handler, true);
+document.addEventListener('click',handler, true);
+window.addEventListener('click',handler, true);
+
+
