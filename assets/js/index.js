3cead1c648dcc7c401977d80604d024b0626c636
danil <minakonn9shkoni@gmail.com>
2021-11-01 17:15:53 +0300

    Add color for card

--- assets/js/index.js
+++ assets/js/index.js
@@ -24,6 +24,7 @@ function createActorCards(actor) {
         .join(".")
     )
   );
+  initials.style.backgroundColor = stringToColour(actor.name);
 
   const img = document.createElement("img");
   img.classList.add("cardImage");
@@ -57,7 +58,7 @@ function stringToColour(str) {
   for (let i = 0; i < str.length; i++) {
     hash = str.charCodeAt(i) + ((hash << 5) - hash);
   }
-  let colour = "0";
+  let colour = "#";
   for (let i = 0; i < 3; i++) {
     let value = (hash >> (i + 8)) & 0xff;
     colour += ("00" + value.toString(16)).substr(-2);
