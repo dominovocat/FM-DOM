6d6fd16ef293c28e5b50fc1631cc607722a45b7a
danil <minakonn9shkoni@gmail.com>
2021-11-02 18:37:05 +0300

    Add validation for images

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,24 +2,21 @@
 
 const messageArray = [];
 
-const form = document.getElementById("root-form");
+const form = document.getElementById('root-form');
 
-
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
   const {
     target: { textInput },
   } = e;
   const inputValue = textInput.value.trim();
-  if(textInput.value){
+  const validation = /(^[A-Z][a-z]{2,12} [A-Z][a-z]{1,17}$)/;
+  const validator = /^[A-Z][a-z]{2,12} ([A-Z]\.){2}$/;
+  const validationForImages = /^(\w+|\d+)*\.(jpg|png)$/
+  if (validationForImages.test(inputValue)) {
     messageArray.push(inputValue);
     form.reset();
   }
-  console.log(e.target.textInput.value);
+  console.dir(messageArray);
 });
-function createElement(type, { classNames }, children) {
-  const elem = document.createElement(type);
-  elem.classList.add(...classNames);
-  elem.append(...children);
-  return elem;
-}
+
