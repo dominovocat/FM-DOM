6097b1fd1b80b0411bc38f51705fa33cb1cb13c3
danil <minakonn9shkoni@gmail.com>
2021-11-03 10:20:10 +0300

    Correct validation

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,7 +12,7 @@ form.addEventListener('submit', (e) => {
   const inputValue = textInput.value.trim();
   const validation = /(^[A-Z][a-z]{2,12} [A-Z][a-z]{1,17}$)/;
   const validator = /^[A-Z][a-z]{2,12} ([A-Z]\.){2}$/;
-  const validationForImages = /^(\w+|\d+)*\.(jpg|png)$/
+  const validationForImages = /^(\w+|\.|\-){1,}\.(jpg|png)$/
   if (validationForImages.test(inputValue)) {
     messageArray.push(inputValue);
     form.reset();
