973a8825da862b73b47739f7ba590c70148262ab
danil <minakonn9shkoni@gmail.com>
2021-11-04 10:50:30 +0300

    Add json.file

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,16 @@
 "use strict";
 
-const btn = document.getElementById('btn');
-btn.addEventListener('click', btnHandler);
-function btnHandler(){
+// const btn = document.getElementById('btn');
+// btn.addEventListener('click', btnHandler);
+// function btnHandler(){
 
-}
+// }
 
-const identificator = setTimeout(()=>{
-  console.log('hi')
-},1000)
+// const identificator = setTimeout(()=>{
+//   console.log('hi')
+// },1000)
 
-clearTimeout(identificator);
+// clearTimeout(identificator);
 
 
 // counter();
@@ -24,20 +24,44 @@ clearTimeout(identificator);
 //   }, 1000);
 // }
 
-const array = [1,2,3,4,5,6,7,8,9,10]
-counter2();
-function counter2(){
-  array.forEach(array => {
-    setTimeout(() => {
-      console.log(array)
-    }, 5000);
-  })};
 
-  counter3();
-  function counter3(){
-    setTimeout(() => {
-      array.forEach(e => {
-        console.log()
-      });
-    }, 1000);
+const user = {
+  name: 'Elon',
+  sname: 'Musk',
+  age:50,
+  getFuulName(){
+    return `${this.name} ${this.sname}`;
+  },
+  children:[1,2,3,'one','two'],
+  isAdult:true,
+  hasPet:undefined,
+  isUkraine:null,
+  [Symbol('chepuha')]:123,
+  friends:{
+    friend:'Tom',
+    firend2:'Tim',
   }
+}
+
+const serializeUser = JSON.stringify(user);
+console.log(serializeUser)
+
+const deserializeUser = JSON.parse(serializeUser);
+console.log(deserializeUser);
+
+
+const obj = {
+  string:'',
+  number:123,
+  logic:true,
+  nothink:null,
+  somethink:undefined,
+  massiv:['one','two',1,2,3]
+}
+console.log(obj);
+
+const serializeObj = JSON.stringify(obj);
+console.log(serializeObj)
+
+const deserializeObj = JSON.parse(serializeObj);
+console.log(deserializeObj);
